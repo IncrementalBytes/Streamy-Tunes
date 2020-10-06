cf4a2376854be948552ba5b49cd7fefaf70332f5
IncrementalBytes <befuddledgeek@gmail.com>
2020-10-06 16:13:58 -0700

    Updated copyright; refactored models

--- app/src/main/java/net/whollynugatory/streamytunes/android/ui/fragments/ArtistsFragment.java
+++ app/src/main/java/net/whollynugatory/streamytunes/android/ui/fragments/ArtistsFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Ryan Ward
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package net.whollynugatory.streamytunes.android.ui.fragments;
 
 import android.content.Context;
@@ -16,7 +31,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import net.whollynugatory.streamytunes.android.R;
 import net.whollynugatory.streamytunes.android.Utils;
-import net.whollynugatory.streamytunes.android.db.views.ArtistDetails;
+import net.whollynugatory.streamytunes.android.db.models.ArtistDetails;
 
 import java.util.ArrayList;
 import java.util.Collection;
--- app/src/main/java/net/whollynugatory/streamytunes/android/ui/fragments/SummaryFragment.java
+++ app/src/main/java/net/whollynugatory/streamytunes/android/ui/fragments/SummaryFragment.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2020 Ryan Ward
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package net.whollynugatory.streamytunes.android.ui.fragments;
 
 import android.content.Context;
@@ -19,9 +34,9 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import net.whollynugatory.streamytunes.android.R;
 import net.whollynugatory.streamytunes.android.Utils;
-import net.whollynugatory.streamytunes.android.db.views.AlbumDetails;
-import net.whollynugatory.streamytunes.android.db.views.ArtistDetails;
-import net.whollynugatory.streamytunes.android.db.views.SongDetails;
+import net.whollynugatory.streamytunes.android.db.models.AlbumDetails;
+import net.whollynugatory.streamytunes.android.db.models.ArtistDetails;
+import net.whollynugatory.streamytunes.android.db.models.SongDetails;
 
 import java.util.Collection;
 import java.util.HashMap;
